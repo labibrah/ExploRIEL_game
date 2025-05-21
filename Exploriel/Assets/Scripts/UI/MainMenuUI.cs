4299655c2610814999387fb946c8c89c2da0c1a8
Robert <150496697+Robertuoftcs@users.noreply.github.com>
2025-05-21 10:25:20 -0400

    starting page and main menu

---
+++ Exploriel/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Load the quiz scene
+        Debug.Log("Starting game...");
+    }
+}
